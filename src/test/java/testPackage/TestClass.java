ea4280dcee35073b1d07b61365e67eeac43e45c1
salmaabdelnabi <35731964+salmaabdelnabi@users.noreply.github.com>
2025-06-28 15:04:21 +0300

    implement workshop task #1

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -2,12 +2,16 @@ package testPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestClass {
 
     @Test
-    public void test(){
+    public void checkTitle(){ //Workshop Task #1
         WebDriver driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+        Assert.assertEquals(driver.getTitle(),"Google");
+        driver.quit();
     }
 }
