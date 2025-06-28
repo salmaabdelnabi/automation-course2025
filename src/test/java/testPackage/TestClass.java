31dc7f15ecafdac137d040e3cb2f1e82a5a36465
salmaabdelnabi <35731964+salmaabdelnabi@users.noreply.github.com>
2025-06-28 15:26:17 +0300

    implement workshop task #2

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -1,5 +1,6 @@
 package testPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -12,6 +13,14 @@ public class TestClass {
         WebDriver driver = new ChromeDriver();
         driver.get("https://duckduckgo.com/");
         Assert.assertEquals(driver.getTitle(),"Google");
+        driver.quit();
+    }
+
+    @Test
+    public void checkLogo(){ //Workshop Task #2
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/article/div[1]/div[1]/div[2]/div/header/div/section[1]/a/img")).isDisplayed());
         driver.quit();
     }
 }
