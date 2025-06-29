cb98a3c3eeadb77797a0941adb58d3833c49b4d6
salmaabdelnabi <35731964+salmaabdelnabi@users.noreply.github.com>
2025-06-29 14:39:58 +0300

    - Add before method and after method actions - Declare driver in the class scope

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -4,23 +4,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Test
 public class TestClass {
+    WebDriver driver;
 
-    @Test
-    public void checkTitle(){ //Workshop Task #1
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://duckduckgo.com/");
-        Assert.assertEquals(driver.getTitle(),"Google");
+    @BeforeMethod
+    public void beforeMethod(){
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void AfterMethod(){
         driver.quit();
     }
 
-    @Test
+    public void checkTitle(){ //Workshop Task #1
+        driver.get("https://duckduckgo.com/");
+        Assert.assertEquals(driver.getTitle(),"Google");
+    }
+
     public void checkLogo(){ //Workshop Task #2
-        WebDriver driver = new ChromeDriver();
         driver.get("https://duckduckgo.com/");
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/article/div[1]/div[1]/div[2]/div/header/div/section[1]/a/img")).isDisplayed());
-        driver.quit();
     }
 }
