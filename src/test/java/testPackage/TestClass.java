d6080d80a43ee28417f496ab8219995c419f4aa8
salmaabdelnabi <35731964+salmaabdelnabi@users.noreply.github.com>
2025-07-07 15:33:33 +0300

    added preliminary implementation for workshop task #4

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -3,6 +3,7 @@ package testPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -13,43 +14,63 @@ import java.time.Duration;
 
 @Test
 public class TestClass {
-    WebDriver driver;
+    WebDriver chromeDriver;
+    WebDriver fireFoxDriver;
+    By duckDuckGoSearchBoxLocator = By.id("searchbox_input");
 
     @BeforeMethod
     public void beforeMethod(){
-        driver = new ChromeDriver();
+        chromeDriver = new ChromeDriver();
+        fireFoxDriver = new FirefoxDriver();
     }
 
     @AfterMethod
     public void afterMethod(){
-        driver.quit();
+        chromeDriver.quit();
+        fireFoxDriver.quit();
     }
 
     public void assertPageTitle(){ //Workshop Task #1
-        driver.navigate().to("https://duckduckgo.com/");
-        Assert.assertEquals(driver.getTitle(),"Google");
+        chromeDriver.navigate().to("https://duckduckgo.com/");
+        Assert.assertEquals(chromeDriver.getTitle(),"Google");
     }
 
     public void assertThatLogoIsDisplayed(){ //Workshop Task #2
-        driver.navigate().to("https://duckduckgo.com/");
+        chromeDriver.navigate().to("https://duckduckgo.com/");
         By duckDuckGoLogoLocator = By.xpath("//div[contains(@class,'Desktop')]//img");
-        Assert.assertTrue(driver.findElement(duckDuckGoLogoLocator).isDisplayed());
+        Assert.assertTrue(chromeDriver.findElement(duckDuckGoLogoLocator).isDisplayed());
     }
 
     public void searchForSeleniumWebDriverAndAssertLink(){ //Workshop Task #3
-        driver.navigate().to("https://duckduckgo.com/");
+        chromeDriver.navigate().to("https://duckduckgo.com/");
 
-        By searchBoxLocator = By.id("searchbox_input");
-        var searchBox = driver.findElement(searchBoxLocator);
+        var searchBox = chromeDriver.findElement(duckDuckGoSearchBoxLocator);
         searchBox.sendKeys("Selenium WebDriver");
         searchBox.submit();
 
-        By firstSearchResultLocator = By.xpath("(//a[@data-testid='result-extras-url-link'])[1]");
+        By firstSearchResultLinkLocator = By.xpath("(//a[@data-testid='result-extras-url-link'])[1]");
 
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(firstSearchResultLocator));
+        WebDriverWait wait = new WebDriverWait(chromeDriver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(firstSearchResultLinkLocator));
 
-        Assert.assertEquals(driver.findElement(firstSearchResultLocator).getAttribute("href"),
+        Assert.assertEquals(chromeDriver.findElement(firstSearchResultLinkLocator).getAttribute("href"),
                 "https://www.selenium.dev/documentation/webdriver/");
+    }
+
+    public void searchForTestNGAssertFourthResultText(){ //Workshop Task #4
+        fireFoxDriver.navigate().to("https://duckduckgo.com/");
+
+        var searchBox = fireFoxDriver.findElement(duckDuckGoSearchBoxLocator);
+        searchBox.sendKeys("TestNG");
+        searchBox.submit();
+
+        By fourthSearchResultTextLocator = By.xpath("(//a[@data-testid='result-title-a'])[4]/span");
+
+        WebDriverWait wait = new WebDriverWait(fireFoxDriver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(fourthSearchResultTextLocator));
+
+        Assert.assertEquals(fireFoxDriver.findElement(fourthSearchResultTextLocator).getText(),
+                "TestNG Tutorial");
+
     }
 }
